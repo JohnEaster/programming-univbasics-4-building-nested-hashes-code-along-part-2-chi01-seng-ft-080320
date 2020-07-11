0eec7038a71e23f91b3785d10fc9da48cc3c96cc
John Easter <john@owltestprep.com>
2020-07-11 16:43:54 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -55,7 +55,7 @@ def add_characters
    #   - The first hash should have the :name key set to "Prince Escalus" 
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
-epic_tragedy[:addiitonal_characters] = []
+epic_tragedy[:addiitonal_characters] => []
 epic_tragedy[:addiitonal_characters] << {:name => "Prince Escalus"}
 epic_tragedy[:addiitonal_characters] << {:name => "Apothecary"}
    # After your code, the following line will return the altered hash
